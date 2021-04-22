22dfc031b91334564df891a05e22b2791b63c63e
lucasstarsz <andrewrcdey@gmail.com>
2021-04-22 08:52:30 -0400

    Update FastJ Website content to deploy

--- fastj/assets/js/7851fb52.a7ee799f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{97:function(e){e.exports=JSON.parse('{"allTagsPath":"/news/tags","slug":"hello","name":"hello","count":2,"permalink":"/news/tags/hello"}')}}]);
